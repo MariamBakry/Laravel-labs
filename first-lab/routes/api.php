db3f51ff32fb98a216d3c7c0a4a9cdec7a725a5a
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-04-01 08:30:19 +0200

    Authentication middleware with Mobile Application Authentication

--- first-lab/routes/api.php
+++ first-lab/routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Api\PostController as ApiPostController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +23,37 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('posts',[\App\Http\Controllers\Api\PostController::class, 'index']);
-Route::get('posts/{post}',[\App\Http\Controllers\Api\PostController::class, 'show']);
-Route::post('posts',[\App\Http\Controllers\Api\PostController::class, 'store']);
+
+
+//only auth will make laravel look at session and cookies
+//with sanctum => will use tokens
+Route::group(['middleware' => ['auth:sanctum']],function () {
+    Route::get('posts', [\App\Http\Controllers\Api\PostController::class, 'index']);
+    Route::get('posts/{post}', [\App\Http\Controllers\Api\PostController::class, 'show']);
+    Route::post('posts', [\App\Http\Controllers\Api\PostController::class, 'store']);
+});
 
 
 // if($request->header('Accept') == 'application/pdf'){
 //     return the pdf;
 // }
+
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
